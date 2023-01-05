4fbaf4a13ff4f34aab76c20f6a40b14d3877173c
DgrinderHZ <hassanzekkouri@gmail.com>
2023-01-05 17:42:41 +0100

    delete

--- details.php
+++ details.php
@@ -12,6 +12,26 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
+ // la partie suppression d'un etudiant
+ if (isset($_POST['cin_delete'])) {
+    $cin = mysqli_real_escape_string($conn, $_POST['cin_delete']);
+
+    // enregistrer la requete dans une variable
+    $sql = "DELETE FROM etudiant WHERE cin = '$cin'";
+
+    // executer la requetes
+   if(mysqli_query($conn, $sql)){
+    // fermer la connexion
+    mysqli_close($conn);
+    // c'est supprimé, il allez à index.php
+    header('Location: index.php');
+   }else{
+    echo "erreur";
+   }
+    // fermer la connexion
+    mysqli_close($conn);
+ }
+
  // Recuperer les donnees de GET
  if (isset($_GET['cin'])) {
     $cin = mysqli_real_escape_string($conn, $_GET['cin']);
@@ -46,7 +66,13 @@ if ($conn->connect_error) {
        </h4>
        <p>Né le: <?php echo htmlspecialchars($etudiant['date_naissance']); ?></p>
        <p>Email: <?php echo htmlspecialchars($etudiant['email']); ?></p>
-    <?php else: ?>
+        <a href="#" class="btn brand z-depth-0">Modifier</a>
+       <!-- DELETE form: formulaire de suppression -->
+        <form action="details.php" method="post">
+            <input hidden type="text" name="cin_delete" value="<?php echo htmlspecialchars($etudiant['cin']); ?>">
+            <input type="submit" name="submit" value="Supprimer" class="btn brand z-depth-0">
+        </form>
+       <?php else: ?>
         <p>no data</p>
     <?php endif; ?>
 </div>
